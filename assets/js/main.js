b9183f61d8c5e8933547af54b1b0f4e44bed8eb5
Попович Екатерина <71285738+qnopka@users.noreply.github.com>
2021-03-22 21:36:11 +0200

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 function updateTextInput(val) {
     document.getElementById('textInput').value=val;
 }
+
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+    this.querySelectorAll(".layer").forEach(layer => {
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/100
+        const y = (window.innerHeight - e.pageY*speed)/100
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
